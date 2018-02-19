3e91f1cbda1c60f86137f2477b02534b0516a2d5
rtgiles <rtgiles@msn.com>
2018-02-19 16:54:49 -0600

    2nd version

--- seeders/burger_seeder.js
+++ seeders/burger_seeder.js
@@ -3,17 +3,18 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
 
-    // Add seeded burgers to database 
+    // Add seeded burgers to database (note that the date needs to be manually added here)
     return queryInterface.bulkInsert('burgers', [
       {burger_name: 'The Wimpy', devoured: false, createdAt: new Date(), updatedAt: new Date()},
       {burger_name: 'The Royal', devoured: false, createdAt: new Date(), updatedAt: new Date()},
-      {burger_name: 'The Blue Cheese King', devoured: false, createdAt: new Date(), updatedAt: new Date()}
+      {burger_name: 'The Bleu Cheese King', devoured: false, createdAt: new Date(), updatedAt: new Date()},
     ], {});
+
   },
 
   down: function (queryInterface, Sequelize) {
     
-    // Remove the seeded burgers 
+    // Remove the seeded burgers (note the "{truncate: true}", which resets the primary keys)
     return queryInterface.bulkDelete('burgers', null, {truncate : true});
     
   }
